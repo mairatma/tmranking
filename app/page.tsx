cfdeb00afe67d93f6a7bf335c7d056c204b8da3c
Maira Bello <maira.tma@gmail.com>
2026-01-03 15:05:24 -0300

    Add page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from 'next';
 import { connection } from 'next/server';
 
 import { RankingPage } from './_ranking/components/RankingPage';
+
+export const metadata: Metadata = {
+  title: 'CBTM Ranking',
+};
 
 export default async function Home() {
   await connection();

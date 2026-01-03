b48c4441267774c1f1459532b21d9e843519152b
Maira Bello <maira.tma@gmail.com>
2026-01-03 13:33:33 -0300

    Improve responsive visual and enrich category data

--- app/_ranking/categories.ts
+++ app/_ranking/categories.ts
@@ -1,194 +1,308 @@
-export const AVAILABLE_CATEGORIES = [
-  {
-    value: '70',
-    label: 'ABSOLUTO A (FEM)',
-  },
-  {
-    value: '71',
-    label: 'ABSOLUTO B (FEM)',
-  },
-  {
-    value: '72',
-    label: 'ABSOLUTO C (FEM)',
-  },
-  {
-    value: '112',
-    label: 'ABSOLUTO D (FEM)',
-  },
-  {
-    value: '267',
-    label: 'SUB-07 FEM',
-  },
-  {
-    value: '65',
-    label: 'SUB-09 FEM',
-  },
-  {
-    value: '113',
-    label: 'SUB-11 FEM',
-  },
-  {
-    value: '66',
-    label: 'SUB-13 FEM',
-  },
-  {
-    value: '67',
-    label: 'SUB-15 FEM',
-  },
-  {
-    value: '68',
-    label: 'SUB-19 FEM',
-  },
-  {
-    value: '271',
-    label: 'SUB-17 FEM',
-  },
-  {
-    value: '69',
-    label: 'SUB-21 FEM',
-  },
-  {
-    value: '77',
-    label: 'ADULTO (FEM)',
-  },
-  {
-    value: '246',
-    label: 'LADY 30',
-  },
-  {
-    value: '247',
-    label: 'LADY 35',
-  },
-  {
-    value: '248',
-    label: 'VETERANO 40 FEM',
-  },
-  {
-    value: '249',
-    label: 'VETERANO 45 FEM',
-  },
-  {
-    value: '250',
-    label: 'VETERANO 50 FEM',
-  },
-  {
-    value: '251',
-    label: 'VETERANO 55 FEM',
-  },
-  {
-    value: '252',
-    label: 'VETERANO 60 FEM',
-  },
-  {
-    value: '253',
-    label: 'VETERANO 65 FEM',
-  },
-  {
-    value: '254',
-    label: 'VETERANO 70 FEM',
-  },
-  {
-    value: '255',
-    label: 'VETERANO 75 FEM',
-  },
-  {
-    value: '110',
-    label: 'ABSOLUTO D (MAS)',
-  },
-  {
-    value: '111',
-    label: 'ABSOLUTO E (MAS)',
-  },
+export enum Gender {
+  Female = 'Female',
+  Male = 'Male',
+}
+
+export enum CategoryType {
+  Absolute = 'Absolute',
+  Youth = 'Youth',
+  Senior = 'Senior',
+}
+
+interface Category {
+  value: string;
+  label: string;
+  gender: Gender;
+  type: CategoryType;
+}
+
+export const AVAILABLE_CATEGORIES: Category[] = [
   {
     value: '57',
     label: 'ABSOLUTO A (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Absolute,
   },
   {
     value: '58',
     label: 'ABSOLUTO B (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Absolute,
   },
   {
     value: '59',
     label: 'ABSOLUTO C (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '110',
+    label: 'ABSOLUTO D (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '111',
+    label: 'ABSOLUTO E (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Absolute,
   },
   {
     value: '125',
     label: 'ABSOLUTO F (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Absolute,
   },
   {
     value: '266',
     label: 'SUB-07 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '52',
     label: 'SUB-09 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '120',
     label: 'SUB-11 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '53',
     label: 'SUB-13 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '54',
     label: 'SUB-15 MAS',
-  },
-  {
-    value: '55',
-    label: 'SUB-19 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '270',
     label: 'SUB-17 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '55',
+    label: 'SUB-19 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '56',
     label: 'SUB-21 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Youth,
   },
   {
     value: '64',
     label: 'ADULTO (MAS)',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '256',
     label: 'SÊNIOR 30',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '257',
     label: 'SÊNIOR 35',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '258',
     label: 'VETERANO 40 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '259',
     label: 'VETERANO 45 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '260',
     label: 'VETERANO 50 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '261',
     label: 'VETERANO 55 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '262',
     label: 'VETERANO 60 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '263',
     label: 'VETERANO 65 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '264',
     label: 'VETERANO 70 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
   {
     value: '265',
     label: 'VETERANO 75 MAS',
+    gender: Gender.Male,
+    type: CategoryType.Senior,
   },
-] as const;
+  {
+    value: '70',
+    label: 'ABSOLUTO A (FEM)',
+    gender: Gender.Female,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '71',
+    label: 'ABSOLUTO B (FEM)',
+    gender: Gender.Female,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '72',
+    label: 'ABSOLUTO C (FEM)',
+    gender: Gender.Female,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '112',
+    label: 'ABSOLUTO D (FEM)',
+    gender: Gender.Female,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '267',
+    label: 'SUB-07 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '65',
+    label: 'SUB-09 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '113',
+    label: 'SUB-11 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '66',
+    label: 'SUB-13 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '67',
+    label: 'SUB-15 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '271',
+    label: 'SUB-17 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '68',
+    label: 'SUB-19 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '69',
+    label: 'SUB-21 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Youth,
+  },
+  {
+    value: '77',
+    label: 'ADULTO (FEM)',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '246',
+    label: 'LADY 30',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '247',
+    label: 'LADY 35',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '248',
+    label: 'VETERANO 40 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '249',
+    label: 'VETERANO 45 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '250',
+    label: 'VETERANO 50 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '251',
+    label: 'VETERANO 55 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '252',
+    label: 'VETERANO 60 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '253',
+    label: 'VETERANO 65 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '254',
+    label: 'VETERANO 70 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+  {
+    value: '255',
+    label: 'VETERANO 75 FEM',
+    gender: Gender.Female,
+    type: CategoryType.Senior,
+  },
+];
--- app/_ranking/components/RankingCardsList.tsx
+++ app/_ranking/components/RankingCardsList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card, Heading, Text } from '@chakra-ui/react';
+import { Card, FormatNumber, Heading, Stat, Text } from '@chakra-ui/react';
 
 import { RankingEntry } from '../types';
 
@@ -14,21 +14,24 @@ export const RankingCardsList = ({ rankings }: Props) => {
       {rankings.map((item) => (
         <Card.Root key={item.rank} hideFrom="sm" mt="4">
           <Card.Header>
-            <Heading size="lg">
+            <Heading size="xl">
               {' '}
               <Text display="inline" color="teal.500">
-                {item.rank}
+                #{item.rank}
               </Text>{' '}
-              - {item.name}
+              {item.name}
             </Heading>
           </Card.Header>
           <Card.Body>
-            <Text textStyle="md" fontWeight="medium" mb="2">
-              Pontos: {item.points}
-            </Text>
-            <Text color="fg.muted">
-              {item.club} - {item.state}
-            </Text>
+            <Stat.Root>
+              <Stat.Label>Pontos </Stat.Label>
+              <Stat.ValueText>
+                <FormatNumber value={item.points} style="decimal" />
+              </Stat.ValueText>
+              <Stat.HelpText>
+                {item.club} - {item.state}
+              </Stat.HelpText>
+            </Stat.Root>
           </Card.Body>
         </Card.Root>
       ))}

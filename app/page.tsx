f4f4ae4ba2f29221d0d04e82d292b144c44fb75f
Maira Bello <maira.tma@gmail.com>
2026-01-03 14:47:54 -0300

    Fix suspense error while deploying

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react';
+
 import { RankingPage } from './_ranking/components/RankingPage';
 
 export default function Home() {
-  return <RankingPage />;
+  return (
+    <Suspense fallback={<>Carregando...</>}>
+      <RankingPage />
+    </Suspense>
+  );
 }

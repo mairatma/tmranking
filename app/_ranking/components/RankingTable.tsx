54b6ba77e2e73230f93f3eb5ccb1da462b862ef6
Maira Bello <maira.tma@gmail.com>
2026-01-04 10:25:31 -0300

    Use simpler link approach

--- app/_ranking/components/RankingTable.tsx
+++ app/_ranking/components/RankingTable.tsx
@@ -3,6 +3,7 @@
 import { Box, Flex, LinkBox, LinkOverlay, Table, Text } from '@chakra-ui/react';
 
 import { RankingEntry } from '../types';
+import Link from 'next/link';
 
 interface Props {
   category: string;
@@ -11,7 +12,7 @@ interface Props {
 
 export const RankingTable = ({ category, rankings }: Props) => {
   return (
-    <Table.Root size="sm" interactive mt="4" overflow="hidden">
+    <Table.Root size="sm" interactive mt="4">
       <Table.Header>
         <Table.Row>
           <Table.ColumnHeader>Nome</Table.ColumnHeader>
@@ -20,27 +21,34 @@ export const RankingTable = ({ category, rankings }: Props) => {
       </Table.Header>
       <Table.Body>
         {rankings.map((item) => (
-          <LinkBox as={Table.Row} key={item.rank} transform="scale(1)">
-            <Table.Cell>
-              <Flex gap="2">
-                <Text fontWeight="bold" color="teal.500">
-                  #{item.rank}
-                </Text>{' '}
-                <Box>
-                  <LinkOverlay
-                    href={`https://app.cbtm.org.br/iUI/Site/RankingResultadoDetalhe?Categoria=${category}&Ano=2026&Associado=${item.id}&Tipo=O&UF=BR&Colocacao=${item.rank}&Pontos=${item.points}`}
-                    target="_blank"
-                  >
+          <Table.Row key={item.rank}>
+            <Table.Cell p="0">
+              <Link
+                href={`https://app.cbtm.org.br/iUI/Site/RankingResultadoDetalhe?Categoria=${category}&Ano=2026&Associado=${item.id}&Tipo=O&UF=BR&Colocacao=${item.rank}&Pontos=${item.points}`}
+                target="_blank"
+              >
+                <Flex gap="2" p="2">
+                  <Text fontWeight="bold" color="teal.500">
+                    #{item.rank}
+                  </Text>{' '}
+                  <Box>
                     <Text fontWeight="bold">{item.name}</Text>
-                  </LinkOverlay>
-                  <Text textStyle="xs" color="fg.muted" fontWeight="normal">
-                    {item.club} - {item.state}
-                  </Text>
-                </Box>
-              </Flex>
+                    <Text textStyle="xs" color="fg.muted" fontWeight="normal">
+                      {item.club} - {item.state}
+                    </Text>
+                  </Box>
+                </Flex>
+              </Link>
             </Table.Cell>
-            <Table.Cell textAlign="end">{item.points}</Table.Cell>
-          </LinkBox>
+            <Table.Cell textAlign="end" p="0">
+              <Link
+                href={`https://app.cbtm.org.br/iUI/Site/RankingResultadoDetalhe?Categoria=${category}&Ano=2026&Associado=${item.id}&Tipo=O&UF=BR&Colocacao=${item.rank}&Pontos=${item.points}`}
+                target="_blank"
+              >
+                <Box p="2">{item.points}</Box>
+              </Link>
+            </Table.Cell>
+          </Table.Row>
         ))}
       </Table.Body>
     </Table.Root>

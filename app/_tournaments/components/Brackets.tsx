31495d954a6b5c3b8b1d8b934cd36bf9bec3c558
Maira Bello <maira.tma@gmail.com>
2026-01-11 15:30:10 -0300

    Add brackets page with mock data

--- app/_tournaments/components/Brackets.tsx
+++ app/_tournaments/components/Brackets.tsx
@@ -1,4 +1,5 @@
 import { Spinner } from '@chakra-ui/react';
+import { Bracket, IRoundProps } from 'react-brackets';
 
 import { useTournamentBrackets } from '@/app/_ranking/hooks/useTournamentBrackets';
 
@@ -7,14 +8,42 @@ interface Props {
   categoryId: string;
 }
 
-export const Brackets = ({ id, categoryId }: Props) => {
-  const { data, error } = useTournamentBrackets(id, categoryId);
+const rounds: IRoundProps[] = [
+  {
+    title: 'Round one',
+    seeds: [
+      {
+        id: 1,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+      },
+      {
+        id: 2,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+      },
+    ],
+  },
+  {
+    title: 'Round two',
+    seeds: [
+      {
+        id: 3,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+    ],
+  },
+];
 
-  console.log('Brackets DATA', data, error);
+export const Brackets = ({ id, categoryId }: Props) => {
+  const { data } = useTournamentBrackets(id, categoryId);
+
+  console.log('Brackets DATA', data);
 
   if (!data) {
     return <Spinner />;
   }
 
-  return <div>WIP</div>;
+  return <Bracket mobileBreakpoint={0} rounds={rounds} />;
 };

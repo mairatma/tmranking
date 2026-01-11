e46450de34c5726e943fef15f3a0110b2c3ca7d6
Maira Bello <maira.tma@gmail.com>
2026-01-11 08:29:52 -0300

    Add UI for searching tournament by id

--- app/_tournaments/components/TournamentsPage.tsx
+++ app/_tournaments/components/TournamentsPage.tsx
@@ -1,5 +1,28 @@
-import { Stack } from '@chakra-ui/react';
+'use client';
+
+import { Button, Group, Input, Stack } from '@chakra-ui/react';
+import { FormEventHandler, useState } from 'react';
 
 export const TournamentsPage = () => {
-  return <Stack>WIP</Stack>;
+  const [tournamentId, setTournamentId] = useState<string | null>(null);
+  const handleSearch: FormEventHandler = (e) => {
+    console.log('Will search', tournamentId);
+    e.preventDefault();
+  };
+
+  return (
+    <Stack>
+      <form onSubmit={handleSearch}>
+        <Group attached w="full" maxW="sm">
+          <Input
+            placeholder="ID do torneio"
+            name="tournamentId"
+            value={tournamentId || ''}
+            onChange={(e) => setTournamentId(e.target.value)}
+          />
+          <Button type="submit">Procurar</Button>
+        </Group>
+      </form>
+    </Stack>
+  );
 };

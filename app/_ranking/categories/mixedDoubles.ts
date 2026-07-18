ce0bc3ee7e63b20a5c360021bbb2a1460e3bca9f
Maira Bello <maira.tma@gmail.com>
2026-07-18 07:21:20 -0300

    feat: add missing file

---
+++ app/_ranking/categories/mixedDoubles.ts
@@ -0,0 +1,46 @@
+import { CategoryType, Gender } from './types';
+
+export const MIXED_DOUBLES_CATEGORIES = [
+  {
+    value: '150',
+    label: 'ABSOLUTO A (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '155',
+    label: 'ABSOLUTO B (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '156',
+    label: 'ABSOLUTO C (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '157',
+    label: 'ABSOLUTO D (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '316',
+    label: 'ABSOLUTO E (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Absolute,
+  },
+  {
+    value: '289',
+    label: 'ADULTO (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Ranking,
+  },
+  {
+    value: '314',
+    label: 'SUB-17 (DUPLAS MISTAS)',
+    gender: Gender.Mixed,
+    type: CategoryType.Ranking,
+  },
+];
